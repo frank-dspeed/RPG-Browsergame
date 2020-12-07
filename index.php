2c607d1073cbe942c55980a5bd60441589a16ad2
Frank Lemanschik <frank@dspeed.eu>
2020-12-07 06:18:33 +0100

    Update

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html>
+<!-- 
+  Flow:
+  Look if we got a PHP Session if we did not got that try to get the value of our session
+  When we do not got a PHP Session we get One
 
+  Look if we are Logged in if we did not got that we need to render the login screen
+
+  Get Current Game Component and display it.
+
+
+
+
+-->
 <head>
   <title>Propania</title>
   <meta charset="utf-8">
